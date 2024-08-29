4572b9c2f6927f9d2e615af8de0a07b2774cc0de
Etogo <modesteetogo1@gmail.com>
2024-08-29 15:27:02 +0100

    Ajout des fichiers sources, deja affectes respectivement du code Js et Autres.

---
+++ script.js
@@ -0,0 +1,45 @@
+/*-----------Variables, Constantes et types de données-----------------------*/
+window.alert("Bienvenue sur Auto-Formation !");
+
+/* Effectuons un test sur les operateurs disponibles en Js */
+
+let val1 = 17; 
+let currentIndex = 1;
+let val2 = null;
+
+val2 = val1 == 17;
+val3 = val1 === 5;
+val4 = val1 != null;
+val5 = val1 !== 17;
+val6 = val1 !== '17';
+
+let newElem = undefined;
+
+var val3; var val4; var val5; var val6;
+
+console.log(val2 + "\n" + val3 + '\n' + val4 + '\n' + val5 + '\n' + val6);
+
+// let resultat = function (arg){
+//     do{
+//         let arg = document.createElement('p');
+//         arg.setAttribute('id', `${"b" + currentIndex}`);
+//         currentIndex++;
+//     }while(currentIndex < 5)
+//     return null;
+// }
+// resultat(newElem);
+
+// newElem.document.getElementById('b1').innerHTML = `Le resultat pour le test 1 est : ${val2}`;
+// newElem.document.getElementById('b2').innerHTML = `Le resultat pour le test 2 est : ${val3}`;
+// newElem.document.getElementById('b3').innerHTML = `Le resultat pour le test 3 est : ${val4}`;
+// newElem.document.getElementById('b4').innerHTML = `Le resultat pour le test 4 est : ${val5}`;
+// newElem.document.getElementById('b5').innerHTML = `Le resultat pour le test 5 est : ${val6}`;
+
+// console.log(typeof(newElem));
+
+
+// const _Live = 10;
+
+// window.alert(`Pour le debut de la partie, votre nombre de vie est egal a : ${_Live}`);
+
+/*--------------------------Structures & Autres-----------------------*/
